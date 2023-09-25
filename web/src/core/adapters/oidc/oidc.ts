634fa2d5ba47d99f08455c775ac3327ce2e3d023
Anders Lunde <8294494+Andilun@users.noreply.github.com>
2023-09-25 10:39:00 +0200

    Forward prompt=consent to the Keycloak authorization endpoint (#4)

--- web/src/core/adapters/oidc/oidc.ts
+++ web/src/core/adapters/oidc/oidc.ts
@@ -67,9 +67,13 @@ export async function createOidc(params: {
             "value": configHash
         });
 
+        // A Google `refresh_token` is only provided during the initial user authorization process, unless prompt is set to consent.
+        // See: https://stackoverflow.com/questions/10827920/not-receiving-google-oauth-refresh-token/10857806#10857806
+        // Without a Google refresh token, Keycloak/oidc provider is unable to refresh the Google access token during token exchange.
         await userManager.signinRedirect({
             redirect_uri,
-            "redirectMethod": "replace"
+            "redirectMethod": "replace",
+            "prompt": "consent"
         });
         return new Promise<never>(() => {});
     };
