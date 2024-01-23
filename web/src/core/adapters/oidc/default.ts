4a8b4108d3a1dacc767e5f8bc20b65dc4a6e5515
John Kasper Svergja <johnkasper.svergja@ssb.no>
2024-01-23 12:30:57 +0100

    Remove obsolete field

--- web/src/core/adapters/oidc/default.ts
+++ web/src/core/adapters/oidc/default.ts
@@ -12,7 +12,6 @@ export async function createOidc(params: {
         issuerUri,
         clientId,
         transformUrlBeforeRedirect,
-        "log": console.log,
         "getExtraQueryParams": () => ({
             // A Google `refresh_token` is only provided during the initial user authorization process, unless prompt is set to consent.
             // See: https://stackoverflow.com/questions/10827920/not-receiving-google-oauth-refresh-token/10857806#10857806
